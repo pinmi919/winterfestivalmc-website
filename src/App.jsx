79ac15c8bd749f28515427b4f4f0d52d83a37045
pinmi919 <pinmi2002@gmail.com>
2026-08-11 22:14:19 +0800

    Render about section in homepage flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import News from './components/News';
 import Schedule from './components/Schedule';
 import Requirements from './components/Requirements';
 import Footer from './components/Footer';
 import Snow from './components/Snow';
-import News from './components/News';
-// 1. Import the Music Player
-import MusicPlayer from './components/MusicPlayer'; 
+import MusicPlayer from './components/MusicPlayer';
 
 function App() {
   return (
@@ -15,12 +14,11 @@ function App() {
       <Snow />
       <Navbar />
       <Hero />
+      <About />
       <News />
       <Schedule />
       <Requirements />
       <Footer />
-      
-      {/* 2. Add the Music Player here at the bottom */}
       <MusicPlayer />
     </div>
   );
