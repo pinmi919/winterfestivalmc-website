c940a1671eadc864be2a58a0890f9e1ec7e95656
pinmi919 <pinmi2002@gmail.com>
2026-08-11 23:18:02 +0800

    use base-aware news image paths

--- src/data/news.js
+++ src/data/news.js
@@ -1,3 +1,5 @@
+import { assetUrl } from '../utils/assetUrl';
+
 export const newsItems = [
   {
     id: 1,
@@ -5,7 +7,7 @@ export const newsItems = [
     excerpt: '第一屆 MC Winter Festival 將於 2026 年 11 月 27 日展開，12 週企劃將以創作者交流、共同創作與冬季節慶活動為核心。',
     category: '企劃公告',
     date: '2026/08/11',
-    image: '/pic1.png',
+    image: assetUrl('pic1.png'),
     featured: true,
   },
   {
@@ -14,7 +16,7 @@ export const newsItems = [
     excerpt: '每週活動、每日任務、每週任務、隱藏任務、冬境幣與 Winter Awards 將共同構成今年冬境之約的主要活動體驗。',
     category: '開發進度',
     date: '2026/08/11',
-    image: '/pic2.png',
+    image: assetUrl('pic2.png'),
     featured: false,
   },
 ];
